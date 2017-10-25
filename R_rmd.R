657dc52fe4492e18e9d082328f04bf86544bc15f
Timothy Mastny <tim.mastny@gmail.com>
2017-10-24 19:17:15 -0500

    new .R as markdown

--- R_rmd.R
+++ R_rmd.R
@@ -21,3 +21,17 @@ y <- 3 + 5*x + rnorm(20,0,0.2)
 plot(x,y)
 
 
+#' ## Section 2:
+
+#' Here's some prose in a very special comment. Let's summarize the built-in
+#' dataset `VADeaths`.
+## here is a regular code comment, that will remain as such
+summary(VADeaths)
+
+#' Here's some more prose. I can use usual markdown syntax to make things
+#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
+#' make a Cleveland dot plot from the `VADeaths` data. I even bother to name
+#' this chunk, so the resulting PNG has a decent name.
+#+ dotchart
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
+
